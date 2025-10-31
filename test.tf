de4eee0450f3614c2904e5b548e23574047789eb
heejun <vsv21gml@naver.com>
2025-10-31 14:36:01 +0900

    lambda test terraform 코드추가

---
+++ test.tf
@@ -0,0 +1,21 @@
+
+# test.tf
+
+data "aws_lambda_invocation" "test_proxy_function" {
+  function_name = aws_lambda_function.proxy_function.function_name
+  input         = file("${path.module}/test.json")
+}
+
+output "lambda_test_result" {
+  value = data.aws_lambda_invocation.test_proxy_function.result
+}
+
+resource "null_resource" "test_assertion" {
+  triggers = {
+    response = data.aws_lambda_invocation.test_proxy_function.result
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${data.aws_lambda_invocation.test_proxy_function.result}' | findstr '200'"
+  }
+}
